4c80ca836f555b90762f47954d0b03765451a0b2
Howard <howard_bb@hotmail.com>
2024-06-20 12:28:13 +0800

    refactor: move env to root

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,10 +1,21 @@
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({ path: path.join(__dirname, '../.env') });
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, '../../../.env') });
 
 const config = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
+  db: {
+    host: process.env.MYSQL_SERVER,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    multipleStatements: true,
+  },
 };
 
 export default config;
