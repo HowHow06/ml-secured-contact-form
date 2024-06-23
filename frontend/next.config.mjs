dac1b3f394e05b18cf28c7aef604242cad4fd154
Howard <howard_bb@hotmail.com>
2024-06-23 23:59:34 +0800

    chore: add dependencies and configurations

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_URL: process.env.API_URL, // need to declare for them to work at client side
+  },
+};
 
 export default nextConfig;
