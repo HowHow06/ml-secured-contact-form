88042fc0f6c423f5a521c015e949139fd085d845
Howard <howard_bb@hotmail.com>
2024-06-25 03:54:34 +0800

    fix: rename env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  env: {
-    API_URL: process.env.API_URL, // need to declare for them to work at client side
-  },
 };
 
 export default nextConfig;
--- frontend/src/lib/config/config.ts
+++ frontend/src/lib/config/config.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = process.env.API_URL;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+console.log("API_BASE_URL:", API_BASE_URL);
