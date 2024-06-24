ca8094ba4b3a7fa277b7f2a84c83884ebb13e42a
Howard <howard_bb@hotmail.com>
2024-06-24 23:54:45 +0800

    feat: add stricter signup limiter

--- backend/src/routes/v1/authRoute.ts
+++ backend/src/routes/v1/authRoute.ts
@@ -1,11 +1,25 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import authController from 'src/controllers/authController';
 import { UserLoginDto, UserRegisterDto } from 'src/dtos/userDto';
 import { validateBody } from 'src/middlewares/validator';
 
 const router = express.Router();
 
-router.post('/signup', validateBody(UserRegisterDto), authController.signup);
+const signupLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 5,
+  message:
+    'Too many signup requests from this IP, please try again after 15 minutes',
+  headers: true,
+});
+
+router.post(
+  '/signup',
+  signupLimiter,
+  validateBody(UserRegisterDto),
+  authController.signup,
+);
 router.post('/login', validateBody(UserLoginDto), authController.login);
 router.post('/logout', authController.logout);
 
